0450d46c508d32a6d39bd6dec871917e059c9f42
aaditya2200 <aaditya2200@gmail.com>
2024-01-26 17:33:10 -0700

    Filter Test Code

--- Tests/BloomFilterTest.py
+++ Tests/BloomFilterTest.py
@@ -1,18 +1,42 @@
 from Filter import BloomFilter
 import hashlib
 import zlib
+import random
 
-def sha1_hash(key):
-    return int(hashlib.sha1(key.encode()).hexdigest(), 16)
+def mod_10_hash(key):
+    return key % 10
 
-def md5_hash(key):
-    return int(hashlib.md5(key.encode()).hexdigest(), 16)
+def mod_9_hash(key):
+    return key % 9
 
-def sha256_hash(key):
-    return hashlib.sha256(key.encode()).hexdigest()
+def mod_8_hash(key):
+    return key % 8
 
-def crc32_hash(key):
-    return zlib.crc32(key.encode())
+def mod_7_hash(key):
+    return key % 7
 
-bf = BloomFilter.BloomFilter(5, 1000, 0.1, [sha256_hash, sha1_hash, md5_hash, crc32_hash])
+
+bf = BloomFilter.BloomFilter(5, 1000, 0.1, [mod_7_hash, mod_8_hash, mod_9_hash, mod_10_hash])
 print('Created bloom filter')
+# Test this out
+# Idea: first generate 1000 random numbers, and insert them
+# Next:
+rand_nums = []
+for i in range(0, 1000):
+    rand_nums.append(random.randint(0, 999999))
+
+save_nums = rand_nums[500:]
+for item in rand_nums:
+    bf.insert(item)
+
+for i in range(0, 500):
+    save_nums.append(random.randint(0, 999999))
+
+fp = 0
+
+for item in save_nums:
+    ret = bf.query(item)
+    if item not in rand_nums and ret:
+        fp += 1
+
+print('false positive rate = ', fp / 1000)
