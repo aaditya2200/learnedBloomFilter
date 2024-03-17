c0446494e66454cb49e6dff852369cbb5b55fd2a
aaditya2200 <aaditya2200@gmail.com>
2024-03-16 20:20:11 -0600

    Bloom filter and test file

--- Filter/BloomFilter.py
+++ Filter/BloomFilter.py
@@ -1,7 +1,15 @@
 from array import array
 import math
 import tensorflow as tf
+from tensorflow.keras.layers import Input, Embedding, Conv1D, MaxPooling1D, Flatten, Dense, Dropout
+from tensorflow.keras.models import Model
 import numpy as np
+
+
+def integer_to_binary(id):
+    return np.array([int(bit) for bit in bin(id)[2:].zfill(64)])
+
+
 class BloomFilter:
     def __init__(self, num_hash_functions, num_entries, false_pos_rate, hash_func_list):
         self.num_hash_functions = num_hash_functions
@@ -10,11 +18,47 @@ class BloomFilter:
         self.number_of_bits = int(-1 * (num_entries * math.log(false_pos_rate)) / pow(math.log(2.0), 2))
         self.hash_func_list = hash_func_list
         self.bloom = array('i', [0] * self.number_of_bits)
-        self.model = tf.keras.Sequential([
-            tf.keras.layers.Dense(64, activation='relu', input_shape=(1,)),
-            tf.keras.layers.Dense(32, activation='relu'),
-            tf.keras.layers.Dense(1, activation='sigmoid')
-        ])
+        # self.model = tf.keras.Sequential([
+        #     tf.keras.layers.Dense(64, activation='relu', input_shape=(64,)),
+        #     tf.keras.layers.Dense(32, activation='relu'),
+        #     tf.keras.layers.Dense(units=16, activation='relu'),
+        #     tf.keras.layers.Dense(1, activation='sigmoid')
+        # ])
+        input_size = 64  # Assuming 64-bit input representation
+        embedding_size = 32  # Embedding size for input bits
+        conv_layers = [[64, 4, 2], [32, 3, 2]]  # Convolutional layers: [filters, kernel_size, pooling_size]
+        fully_connected_layers = [32, 1]  # Fully connected layers
+        dropout_p = 0.5  # Dropout probability
+        optimizer = 'adam'
+        loss = 'binary_crossentropy'
+
+        # Define the input layer
+        inputs = Input(shape=(input_size,), name='input', dtype='int64')
+
+        # Embedding layer
+        embedding_layer = Embedding(input_dim=2, output_dim=embedding_size, input_length=input_size)
+        embedded_inputs = embedding_layer(inputs)
+
+        # Convolutional layers
+        x = embedded_inputs
+        for filters, kernel_size, pooling_size in conv_layers:
+            x = Conv1D(filters=filters, kernel_size=kernel_size, activation='relu')(x)
+            x = MaxPooling1D(pool_size=pooling_size)(x)
+
+        # Flatten the output
+        x = Flatten()(x)
+
+        # Fully connected layers
+        for units in fully_connected_layers:
+            x = Dense(units, activation='relu')(x)
+            x = Dropout(dropout_p)(x)
+
+        # Output layer
+        predictions = Dense(1, activation='sigmoid')(x)
+
+        # Build the model
+        self.model = Model(inputs=inputs, outputs=predictions)
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
         self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     def insert(self, key):
@@ -22,8 +66,14 @@ class BloomFilter:
             index = int(func(key) % self.number_of_bits)
             self.bloom[index] = 1
         # Now, lets train our model also.
-        X_train, y_train = np.array([[key]]), np.array([[1]])
+
+        X_train, y_train = np.array([integer_to_binary(key)]), np.array([[1]])
         self.model.train_on_batch(X_train, y_train)
+
+    def train(self, nums):
+        for item in nums:
+            X_train, y_train = np.array([integer_to_binary(item)]), np.array([[0]])
+            self.model.train_on_batch(X_train, y_train)
 
     def query(self, key):
         for func in self.hash_func_list:
@@ -33,5 +83,8 @@ class BloomFilter:
         return True
 
     def query_nn(self, key):
-        prediction = self.model.predict(np.array([[key]]))
-        return (prediction > 0.5).astype(int)
+        prediction = self.model.predict(np.array([integer_to_binary(key)]))
+        if prediction > 0.9:
+            return True
+        else:
+            return self.query(key)
--- Tests/BloomFilterTest.py
+++ Tests/BloomFilterTest.py
@@ -6,6 +6,26 @@ import random
 def mod_10_hash(key):
     return key % 10
 
+def division_hash(key):
+    return key % 997
+
+def multiplication_hash(key):
+    return (key * 2654435761) & 0xFFFFFFFF
+
+def bitwise_xor_hash(key):
+    return key ^ (key >> 16)
+
+def sax_hash(key):
+    key = (~key) + (key << 21)  # key = (key << 21) - key - 1;
+    key = key ^ (key >> 24)
+    key = (key + (key << 3)) + (key << 8)  # key * 265
+    key = key ^ (key >> 14)
+    key = (key + (key << 2)) + (key << 4)  # key * 21
+    key = key ^ (key >> 28)
+    key = key + (key << 31)
+    return key
+
+
 def mod_9_hash(key):
     return key % 9
 
@@ -16,27 +36,29 @@ def mod_7_hash(key):
     return key % 7
 
 
-bf = BloomFilter.BloomFilter(5, 1000, 0.1, [mod_7_hash, mod_8_hash, mod_9_hash, mod_10_hash])
+even_numbers = []
+odd_numbers = []
+
+# Generate even and odd numbers between 0 and 9999
+for num in range(1000):
+    if num % 2 == 0:
+        even_numbers.append(num)
+    else:
+        odd_numbers.append(num)
+
+
+
+bf = BloomFilter.BloomFilter(5, 1000, 0.1, [division_hash, sax_hash, multiplication_hash, bitwise_xor_hash])
 print('Created bloom filter')
-# Test this out
-# Idea: first generate 1000 random numbers, and insert them
-# Next:
-rand_nums = []
-for i in range(0, 1000):
-    rand_nums.append(random.randint(0, 999999))
 
-save_nums = rand_nums[500:]
-for item in rand_nums:
+for item in odd_numbers:
     bf.insert(item)
-
-for i in range(0, 500):
-    save_nums.append(random.randint(0, 999999))
-
+#bf.train(even_numbers)
+nums = odd_numbers + even_numbers
 fp = 0
-
-for item in save_nums:
-    ret = bf.query(item)
-    if item not in rand_nums and ret:
+for item in nums:
+    ret = bf.query_nn(item)
+    if item not in odd_numbers and ret:
         fp += 1
 
 print('false positive rate = ', fp / 1000)
