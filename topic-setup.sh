42070468151185a08dbd917ca83997d33b1f158b
aaditya2200 <aaditya2200@gmail.com>
2024-04-21 12:09:43 -0600

    Removed unwanted lines

--- topic-setup.sh
+++ topic-setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 kafka-topics.sh --create --topic stream-test --bootstrap-server localhost:9092
-kafka-console-producer.sh --topic tstream-test --bootstrap-server localhost:9092
-kafka-console-consumer.sh --topic stream-test --bootstrap-server localhost:9092 --from-beginning
